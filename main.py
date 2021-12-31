d2186a617ed3e7644456fe51a2f22bc4d7201d26
NH-Studios <nicolashunger@outlook.com>
2021-12-31 01:24:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ from pygame.math import Vector2
 import random
 
 
-
-pygame.init()
-color1 = (170, 215, 81)
-color2 = (162, 209, 73)
-
-
 class Snake:
     def __init__(self):
-        self.body = [Vector2(5,10), Vector2(6,10), Vector2(7,10)]
-        self.Weg = []
+        self.body = [Vector2(5,10), Vector2(4,10), Vector2(3,10)]
         self.direction = Vector2(1,0)
+        self.new_part = False
+
+        #images
+
 
     def draw_snake(self):
         for part in self.body:
@@ -24,63 +21,75 @@ class Snake:
             pygame.draw.rect(surface, 'blue', body_rect)
 
     def move_snake(self):
-        body_copy = self.body[:-1]
-        body_copy.insert(0, body_copy[0] + self.direction)
-        self.body = body_copy[:]
+        if self.new_part == True:
+            body_copy = self.body[:]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
+            self.new_part = False
+        else:
+            body_copy = self.body[:-1]
+            body_copy.insert(0, body_copy[0] + self.direction)
+            self.body = body_copy[:]
 
-
-
-
-
-    def increaseLänge(self):
-        self.länge += 1
+    def add_part(self):
+        self.new_part = True
 
 
 class Food:
     def __init__(self):
-        self.x = random.randint(0, Gridnumber)
-        self.y = random.randint(0, Gridnumber)
-        self.pos = Vector2(self.x, self.y) #Vektor
+        self.respawn()
 
     def draw_food(self):
         food_rect = pygame.Rect(self.pos.x * Gridsize, self.pos.y * Gridsize, Gridsize, Gridsize)
-        pygame.draw.rect(surface, 'red', food_rect)
+        surface.blit(apple, food_rect)
+        #pygame.draw.rect(surface, 'red', food_rect)
+
+    def respawn(self):
+        self.x = random.randint(0, Gridnumber)
+        self.y = random.randint(0, Gridnumber)
+        self.pos = Vector2(self.x, self.y)  # Vektor
+
+
+class Main:
+    def __init__(self):
+        self.snake = Snake()
+        self.food = Food()
+
+    def update(self):
+        self.snake.move_snake()
+        self.check_collision()
+        self.check_fail()
+
+    def draw_elements(self):
+        self.food.draw_food()
+        self.snake.draw_snake()
+
+    def check_collision(self):
+        if self.food.pos == self.snake.body[0]: #Food & Head of snake at same position
+            self.food.respawn()
+            self.snake.add_part()
+
+    def check_fail(self):
+        if not 0 <= self.snake.body[0].x <= Gridnumber or not 0 <= self.snake.body[0].y <= Gridnumber:
+            self.game_over()
+
+        for part in self.snake.body[1:]:#All elements except for the head
+            if part == self.snake.body[0]:
+                self.game_over()
+
+
+    def game_over(self):
+        pygame.quit()
+        sys.exit()
 
 
 
-
-
-class Head:
-    def __init__(self, speedx, speedy, position):
-        self.speedx = speedx
-        self.speedy = speedy
-        self.position  = position
-
-
-
-class Tile:
-    def __init__(self, xKoordinate, yKoordinate, position):
-        self.xKoordinate = xKoordinate
-        self.yKoordinate = yKoordinate
-        self.position = position
-
-    def bewegen(self, xSchritt, ySchritt):
-        self.Weg.append(self.position)
-        xKoordinate += xSchritt
-        yKoordinate += ySchritt
-
-
-
-
-#settings
-pygame.init()
-clock = pygame.time.Clock()
-surface = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("Sneik")
 
 Gridnumber = 19
 Gridsize = 40
 def grid():#Grid, 19 tiles
+    color1 = (170, 215, 81)
+    color2 = (162, 209, 73)
     surface.fill(color1)
     gridy = 0
     gridx = 0
@@ -101,9 +110,21 @@ def grid():#Grid, 19 tiles
         gridx += 80
 
 
-# TestObjekt
-test_essen = Food()
-test_snake = Snake()
+
+
+pygame.init()
+
+#settings
+pygame.init()
+clock = pygame.time.Clock()
+surface = pygame.display.set_mode((800, 800))
+pygame.display.set_caption("Sneik")
+#images
+apple = pygame.image.load('assets/Graphics/apple.png').convert_alpha()
+
+
+#main class
+main_game = Main()
 
 #Movement -> 150ms
 SCREEN_UPDATE = pygame.USEREVENT
@@ -130,31 +151,42 @@ while True:
             sys.exit()
 
         if event.type == SCREEN_UPDATE:
-            test_snake.move_snake()
+            main_game.update()
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 pause()
 
             if event.key == pygame.K_DOWN:
-                pass
+                if main_game.snake.direction == Vector2(0,-1):
+                    pass
+                else:
+                    main_game.snake.direction = Vector2(0,1)
 
             if event.key == pygame.K_RIGHT:
-                pass
+                if main_game.snake.direction == Vector2(-1,0):
+                    pass
+                else:
+                    main_game.snake.direction = Vector2(1,0)
 
-            if event.type == pygame.K_LEFT:
-                pass
+            if event.key == pygame.K_LEFT:
+                if main_game.snake.direction == Vector2(1,0):
+                    pass
+                else:
+                    main_game.snake.direction = Vector2(-1,0)
 
-            if event.type == pygame.K_UP:
-                pass
+            if event.key == pygame.K_UP:
+                if main_game.snake.direction == Vector2(0,1):
+                    pass
+                else:
+                    main_game.snake.direction = Vector2(0,-1)
+
 
 
     grid()
-    test_essen.draw_food()
-    test_snake.draw_snake()
+    main_game.draw_elements()
 
     pygame.display.update()
     clock.tick(60)
 
-        #pygame.draw.rect(surface, Quadrat, (400, 400))
 
